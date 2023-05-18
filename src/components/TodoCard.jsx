b696fa955fa110e049ec6c9d8bce7af8acd6d281
Victor S <joaovictorsal777@gmail.com>
2023-05-18 00:36:41 -0300

    finalizado TodoList

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -5,31 +5,39 @@ import { Todos } from "./Todos"
 
 
 export const TodoCard = () => {
+    
  
     const [tasks, setTasks] = useState([
     ])
 
+    localStorage.getItem(setTasks)
 
     const addTodo = (task) => {
+        
         setTasks([...tasks, 
             {id:Math.random(), task:task}
         ])
-        console.log(tasks)
+        localStorage.setItem(tasks, task)
+        
     }
 
     const deleteTask = (id) => {
         setTasks(tasks.filter(task => task.id !== id))
+        
+        
     }
 
     const taskComplete = (id) => {
         setTasks(tasks.map(task => task.id === id ?
             {...task, completed: !task.completed} : task))
+
     }
 
     return (
         <div className='todo-card'>
-            <h1>Lista de tarefas</h1>
+            <h2>Lista de tarefas</h2>
             <FormTodo addTodo={addTodo}/>
+            {tasks.length > 0 && <h5>tarefas:</h5>}
             {tasks.map((task, index) => (
                 <Todos 
                 task={task} 
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,15 +1,16 @@
-import { IoIosCloseCircleOutline, IoMdCheckmarkCircle } from 'react-icons/io'
+import { IoIosCloseCircleOutline,IoIosCheckmarkCircleOutline} from 'react-icons/io'
 
 export const Todos = ({task, deleteTask, taskComplete}) =>{
 
 
     return(
-        <div className='todos'>
-            <p onClick={() => taskComplete(task.id) }
+        <div  className='todos'>
+            <p  
             className={`${task.completed ? 'completed' : ""}`}>
             {task.task}</p>
             <div 
             className='todo-icons'>
+                <IoIosCheckmarkCircleOutline onClick={() => taskComplete(task.id)}/>
                 <IoIosCloseCircleOutline className='delete-icon'
                 onClick={() => deleteTask(task.id)}/>
             </div>
